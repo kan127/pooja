b3e3cbb0a1a81b97e8fe664e093a48971513fc4e
kanda <kanda@LAPTOP-NO349DAQ>
2022-01-29 23:19:50 +0530

    pooja testing

---
+++ Repositoryproject/src/repopackage/Repoclass.java
@@ -0,0 +1,18 @@
+package repopackage;
+
+public class Repoclass {
+
+	public int Sum(int b,int c) {
+		int a;
+		a=b+c;
+		return(a);
+	}
+
+	public static void main(String[] args) {
+		Repoclass jj =new Repoclass();
+		jj.Sum(4,5);
+		
+
+	}
+
+}
